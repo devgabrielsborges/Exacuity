2bf9ac84b31abdbcfbf7c2f7fed293501ce00c57
devgabrielsborges <devgabrielsborges@gmail.com>
2025-01-30 19:41:11 -0300

    feat: add ConfigurationsActivity and layout for configuration settings, including grid item layout

---
+++ app/src/main/java/com/example/exacuity/ConfigurationsActivity.java
@@ -0,0 +1,14 @@
+package com.example.exacuity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfigurationsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurations);
+
+        // TODO: Implement configuration settings here
+    }
+}
